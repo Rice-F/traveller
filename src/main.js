3a4e914dce88c99a8753c408eb2095204efff173
Rice-F <270884083@qq.com>
2018-05-15 15:23:40 +0800

    project init

--- src/main.js
+++ src/main.js
@@ -3,8 +3,14 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 移动端300ms点击优化
+import fastClick from 'fastclick'
+import './assets/styles/reset.css'
+// 解决2、3倍屏的1px边框问题
+import './assets/styles/border.css'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
 
 /* eslint-disable no-new */
 new Vue({
