f23de917184ae3172066430089adbbebcdf15136
Rice-F <2604211051@qq.com>
2018-05-16 14:20:02 +0800

    add home-header

--- src/main.js
+++ src/main.js
@@ -5,9 +5,10 @@ import App from './App'
 import router from './router'
 // 移动端300ms点击优化
 import fastClick from 'fastclick'
-import './assets/styles/reset.css'
+import 'styles/reset.css'
 // 解决2、3倍屏的1px边框问题
-import './assets/styles/border.css'
+import 'styles/border.css'
+import 'styles/iconfont.css'
 
 Vue.config.productionTip = false
 fastClick.attach(document.body)
---
+++ src/pages/home/Home.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <home-header></home-header>
+  </div>
+</template>
+
+<script>
+import HomeHeader from './componnets/Header'
+export default {
+  name: 'Home',
+  components: {
+    HomeHeader
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
