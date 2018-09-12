edac0c2b2e1369c39f161d6e57874eac3e1f9dba
Rice-F <2604211051@qq.com>
2018-09-12 13:48:22 +0800

    add detail banner animation

--- src/pages/detail/components/Banner.vue
+++ src/pages/detail/components/Banner.vue
@@ -13,21 +13,25 @@
         <div class="banner-title">{{this.sightName}}</div>
       </div>
     </div>
-    <common-gallary
-      :imgs="bannerImages"
-      v-show="showGallary"
-      @close="handleGallaryInvisible"
-    ></common-gallary>
+    <fade-animation>
+      <common-gallary
+        :imgs="bannerImages"
+        v-show="showGallary"
+        @close="handleGallaryInvisible"
+      ></common-gallary>
+    </fade-animation>
   </div>
 
 </template>
 
 <script>
 import CommonGallary from 'common/gallary/Gallary'
+import FadeAnimation from 'common/fade/FadeAnimation'
 export default {
   name: 'DetailBanner',
   components: {
-    CommonGallary
+    CommonGallary,
+    FadeAnimation
   },
   props: {
     sightName: String,
